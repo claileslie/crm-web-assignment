7a941b1df4446b1f65a42716da37d1d32b5ca672
CLAIRE LESLIE <c.leslie@live.ca>
2017-01-24 14:58:00 -0500

    add view all contacts, implement view all contacts

--- crm.rb
+++ crm.rb
@@ -1,9 +1,22 @@
 # Implement the new web-based CRM here.
 # Do NOT copy the CRM class from the old crm assignment, as it won't work at all for the web-based version!
 # You'll have to implement it from scratch.
-require 'sinatra'
+require_relative "contact"
+require "sinatra"
 
-get '/' do
+Contact.create("Prudence","Leslie","prudence@live.ca","mama kitty")
+Contact.create("Oliver","Leslie","oliver@live.ca","daddy kitty")
+Contact.create("Max", "Leslie", "max@live.ca", "baby kitty")
+
+
+
+
+get "/" do
   @crm_app_name = "Claire's CRM"
   erb :index
+end
+
+get "/contacts" do
+  @contact_name = "Contacts"
+  erb :contacts
 end
