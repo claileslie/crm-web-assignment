2b97184e0e3aaffd4499d221d7585972241544ed
CLAIRE LESLIE <c.leslie@live.ca>
2017-01-24 15:37:35 -0500

    complete new contact form

--- crm.rb
+++ crm.rb
@@ -1,22 +1,31 @@
 # Implement the new web-based CRM here.
 # Do NOT copy the CRM class from the old crm assignment, as it won't work at all for the web-based version!
 # You'll have to implement it from scratch.
-require_relative "contact"
-require "sinatra"
+require_relative 'contact'
+require 'sinatra'
 
-Contact.create("Prudence","Leslie","prudence@live.ca","mama kitty")
-Contact.create("Oliver","Leslie","oliver@live.ca","daddy kitty")
-Contact.create("Max", "Leslie", "max@live.ca", "baby kitty")
-
-
+# Contact.create("Prudence","Leslie","prudence@live.ca","mama kitty")
+# Contact.create("Oliver","Leslie","oliver@live.ca","daddy kitty")
+# Contact.create("Max", "Leslie", "max@live.ca", "baby kitty")
 
 
-get "/" do
+
+
+get '/' do
   @crm_app_name = "Claire's CRM"
   erb :index
 end
 
-get "/contacts" do
-  @contact_name = "Contacts"
+get '/contacts' do
+  @contact_name = 'Contacts'
   erb :contacts
+end
+
+get '/contacts/new' do
+  erb :new_contact
+end
+
+post '/contacts' do
+  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+  redirect to('/contacts')
 end
